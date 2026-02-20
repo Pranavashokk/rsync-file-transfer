7a1accd294f47c9f9f8a41a1cdf7e00dd3f9fabd
Pranavashokk <mailpranav2020@gmail.com>
2026-02-20 13:02:37 +0530

    Update rsync script logic

--- myscript.sh
+++ myscript.sh
@@ -13,15 +13,11 @@ echo "r u the sender(1) or reciever(2)"
 read -p "select an option (1-2): " ROLE
 
 case $ROLE in
-    1)
-        
-        install_rsync
+    1)  install_rsync
         mkdir -p ~/rsync/shared-file
         IP_ADDR=$(hostname -I | awk '{print $1}')
-        
         echo "MODE: DESTINATION"
         echo "1. make sure ssh is running: sudo systemctl start ssh , type the command to the sender"
-      
         echo "rsync -avzP /path/to/source/folder/ $(whoami)@${IP_ADDR}:~/rsync/shared-file/"
         ;;
     2)
@@ -32,10 +28,7 @@ case $ROLE in
         read -p "enter destination ip address: " DEST_IP
         read -p "enter  destination username: " DEST_USER
         read -p "enter the FULL path of the folder to send: " SRC_PATH
-        
-        
         echo "rsync -avzP $SRC_PATH ${DEST_USER}@${DEST_IP}:~/rsync/shared-file/"
-        
         read -p "Execute transfer now? (y/n): " CONFIRM
         if [[ $CONFIRM == "y" ]]; then
             rsync -avzP "$SRC_PATH" "${DEST_USER}@${DEST_IP}:~/rsync/shared-file/"
