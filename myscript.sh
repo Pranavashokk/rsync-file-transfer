f385da6f20f1ea0f54ece3539deff7b0d3842f45
Pranavashokk <mailpranav2020@gmail.com>
2026-02-20 12:58:26 +0530

    Update rsync script logic

--- myscript.sh
+++ myscript.sh
@@ -1,15 +1,50 @@
 #!/bin/bash
 
-if ! command -v rsync &> /dev/null; then
-    echo "rsync not found. Attempting to install..."
-    if [ -f /etc/redhat-release ]; then
-        sudo dnf install -y rsync
-    else
-        sudo apt update && sudo apt install -y rsync
+install_rsync() {
+    if ! command -v rsync &> /dev/null; then
+        echo "rsync not found. Installing..."
+        if command -v dnf &> /dev/null; then sudo dnf install -y rsync
+        elif command -v apt &> /dev/null; then sudo apt update && sudo apt install -y rsync
+        fi
     fi
-fi
-mkdir -p ~/rsync/shared-file
-CURRENT_USER=$(whoami)
-IP_ADDR=$(hostname -I | awk '{print $1}')
-echo "RUN THIS ON THE SOURCE COMPUTER:"
-echo "rsync -avz /path/to/source/folder/ ${CURRENT_USER}@${IP_ADDR}:rsync/shared-file/"
+}
+
+echo "Are you the sender or reciever?"
+echo "1) destination  2)sender"
+read -p "select an option (1-2): " ROLE
+
+case $ROLE in
+    1)
+        
+        install_rsync
+        mkdir -p ~/rsync/shared-file
+        IP_ADDR=$(hostname -I | awk '{print $1}')
+        
+        echo "MODE: DESTINATION"
+        echo "1. make sure SSH is running: sudo systemctl start ssh"
+        echo "2. type the command to the sender"
+      
+        echo "rsync -avzP /path/to/source/folder/ $(whoami)@${IP_ADDR}:~/rsync/shared-file/"
+        ;;
+    2)
+       
+        install_rsync
+       
+        echo "MODE: SOURCE"
+        read -p "enter the destination IP address: " DEST_IP
+        read -p "enter the destination Username: " DEST_USER
+        read -p "enter the FULL path of the folder to send: " SRC_PATH
+        
+        
+        echo "rsync -avzP $SRC_PATH ${DEST_USER}@${DEST_IP}:~/rsync/shared-file/"
+        
+        read -p "Execute transfer now? (y/n): " CONFIRM
+        if [[ $CONFIRM == "y" ]]; then
+            rsync -avzP "$SRC_PATH" "${DEST_USER}@${DEST_IP}:~/rsync/shared-file/"
+        fi
+        ;;
+    *)
+        
+        exit 1
+        ;;
+esac
